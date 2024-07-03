dfb4b4d0c9bc3a85742cd84e6fdb5960213bdb03
Gunnar <gunnar.montag@gmx.de>
2024-07-03 12:15:22 +0200

    add ColorForm component to add custom color. add ColorInput component to handle color inputs in ColorForm component.

---
+++ src/Components/ColorForm/ColorForm.jsx
@@ -0,0 +1,32 @@
+import ColorInput from "../ColorInput/ColorInput";
+import "./ColorForm.css";
+
+export default function ColorForm({ onAddColor }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formElements = new FormData(event.target);
+    const data = Object.fromEntries(formElements);
+    onAddColor(data);
+    console.log(data);
+
+    event.target.reset();
+    event.target.role.focus();
+  }
+
+  return (
+    <form className="addColorForm" onSubmit={handleSubmit}>
+      <label htmlFor="role">Role: </label>
+      <input
+        type="text"
+        name="role"
+        id="role"
+        placeholder={"give your color a role..."}
+      />
+      <label htmlFor="hex">Hex: </label>
+      <ColorInput id="hex" defaultValue="#ffffff" />
+      <label htmlFor="contrastText">Contrast Text: </label>
+      <ColorInput id="contrastText" defaultValue="#000000" />
+      <button type="submit">ADD COLOR</button>
+    </form>
+  );
+}
